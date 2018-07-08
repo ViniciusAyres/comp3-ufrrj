8bee7cbf6835c91b6a73da3d82dce72def1a36bb
Daniel Avellar <danielavellar06@gmail.com>
2018-07-08 18:05:35 -0300

    criacao servlet usuario

--- src/main/java/dominio/UsuarioDominio.java
+++ src/main/java/dominio/UsuarioDominio.java
@@ -6,7 +6,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
-public class UsuarioDominio {
+@WebServlet(name = "UsuarioDominio", urlPatterns = "/usuario")
+public class UsuarioDominio extends HttpServlet{
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
 }
