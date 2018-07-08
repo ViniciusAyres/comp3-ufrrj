d173f1f2b64457bc33ef3c89f1d21a15bf1cdd51
Vinicius <viniciusayres.sg@gmail.com>
2018-07-07 22:41:40 -0300

    [update] Remoção do id no Atleta

--- src/dados/entidades/Atleta.java
+++ src/dados/entidades/Atleta.java
@@ -3,27 +3,17 @@ package dados.entidades;
 import java.util.Date;
 
 public class Atleta {
-    public int id;
     public String nome;
     public String matricula;
     public Date dataNascimento;
     public String categoria;
 
-    public Atleta(int id, String nome, String matricula, Date dataNascimento, String categoria) {
+    public Atleta(String nome, String matricula, Date dataNascimento, String categoria) {
 
-        this.id = id;
         this.nome = nome;
         this.matricula = matricula;
         this.dataNascimento = dataNascimento;
         this.categoria = categoria;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getNome() {
