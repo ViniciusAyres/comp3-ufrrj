c16083d41c480f08dc5fd851731af81781cdd42a
Vinicius <viniciusayres.sg@gmail.com>
2018-07-07 22:30:16 -0300

    [add] Classe para criar a conexão com o Derby

---
+++ src/dados/bancos/derbyDB/ConnectionSingleton.java
@@ -0,0 +1,34 @@
+package dados.bancos.derbyDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionSingleton {
+    private static String dbURL = "jdbc:derby://ruralcomp3.dlinkddns.com:1527/NatacaoDB;create=true;";
+    private static Connection connection = null;
+
+    protected ConnectionSingleton(){
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
+            //Get a connection
+            this.connection = DriverManager.getConnection(dbURL);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getInstance() {
+        if(connection == null){
+            new ConnectionSingleton();
+        }
+
+        return connection;
+    }
+}
