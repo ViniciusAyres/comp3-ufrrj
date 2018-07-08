19de509163d62f5cbbacfe638c1af3783510cbf4
Vinicius <viniciusayres.sg@gmail.com>
2018-07-08 00:36:32 -0300

    [update] Implementação de função de busca

--- src/dados/datamappers/AtletaDataMapper.java
+++ src/dados/datamappers/AtletaDataMapper.java
@@ -1,13 +1,15 @@
 package dados.datamappers;
 
 import dados.bancos.derbyDB.ConnectionSingleton;
+import dados.datamappers.interfaces.StringDataMapper;
 import dados.entidades.Atleta;
 
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class AtletaDataMapper {
+public class AtletaDataMapper implements StringDataMapper {
     public boolean criar(Atleta atleta){
         if (atleta == null){
             return true;
@@ -25,6 +27,65 @@ public class AtletaDataMapper {
             statement.setString(2, atleta.getMatricula());
             statement.setDate(3, (Date) atleta.getDataNascimento());
             statement.setString(4, atleta.getCategoria());
+            statement.execute();
+            statement.close();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public Atleta buscarPorMatricula(String matricula){
+        String sql = "SELECT * FROM TABELA WHERE MATRICULA = ?";
+
+        PreparedStatement statement;
+
+        try{
+            statement = (PreparedStatement) ConnectionSingleton.getInstance()
+                    .prepareStatement(sql);
+
+            statement.setString(1, matricula);
+            ResultSet resultSet = statement.executeQuery();
+            Atleta atleta = null;
+
+            if(resultSet.next()){
+                atleta = new Atleta(resultSet.getString("NOME"),
+                        resultSet.getString("MATRICULA"),
+                        resultSet.getDate("DATA_NASCIMENTO"),
+                        resultSet.getString("CATEGORIA"));
+
+            }
+
+            statement.close();
+            resultSet.close();
+            return atleta;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public boolean atualizar(Atleta atleta){
+        if (atleta == null){
+            return true;
+        }
+
+        String sql = "UPDATE ATLETA " +
+                "SET NOME = ?, MATRICULA = ?, DATA_NASCIMENTO = ?, CATEGORIA = ? " +
+                "WHERE MATRICULA = ?";
+        PreparedStatement statement;
+
+        try{
+            statement = (PreparedStatement) ConnectionSingleton.getInstance()
+                    .prepareStatement(sql);
+
+            statement.setString(1, atleta.getNome());
+            statement.setString(2, atleta.getMatricula());
+            statement.setDate(3, (Date) atleta.getDataNascimento());
+            statement.setString(4, atleta.getCategoria());
+            statement.setString(5, atleta.getMatricula());
             statement.execute();
             statement.close();
             return true;
