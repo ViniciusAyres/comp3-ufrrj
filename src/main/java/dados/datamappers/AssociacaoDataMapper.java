f1b1cf55f70981b484b2960fc276c911969db70a
Vinicius <viniciusayres.sg@gmail.com>
2018-07-08 21:09:45 -0300

    [add] Implementaçao dos metodos atualizar e criar

--- src/main/java/dados/datamappers/AssociacaoDataMapper.java
+++ src/main/java/dados/datamappers/AssociacaoDataMapper.java
@@ -1,6 +1,10 @@
 package dados.datamappers;
 
+import dados.bancos.derbyDB.ConnectionSingleton;
+
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class AssociacaoDataMapper {
     public ResultSet buscarPorMatricula(String matricula){
@@ -12,6 +16,52 @@ public class AssociacaoDataMapper {
         }
 
         return  null;
+    }
+
+    public boolean criar(String matricula, String nome, String sigla, String telefone, int idEndereco){
+        String sql = "INSERT INTO ASSOCIACAO (MATRICULA, NOME, SIGLA, TELEFONE, ID_ENDERECO) " +
+                "VALUES (?, ?, ?, ?, ?)";
+
+        try {
+            PreparedStatement statement = (PreparedStatement) ConnectionSingleton.getInstance()
+                    .prepareStatement(sql);
+
+            statement.setString(1, matricula);
+            statement.setString(2, nome);
+            statement.setString(3, sigla);
+            statement.setString(4, telefone);
+            statement.setInt(5, idEndereco);
+            return statement.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    public boolean atualizar(String matricula, String nome, String sigla, String telefone, int idEndereco){
+        String sql = "UPDATE ASSOCIACAO " +
+                "SET NOME = ?, SIGLA = ?, TELEFONE = ?, ID_ENDERECO = ? " +
+                "WHERE MATRICULA = ?";
+
+        try {
+            PreparedStatement statement = (PreparedStatement) ConnectionSingleton.getInstance()
+                    .prepareStatement(sql);
+
+            statement.setString(1, nome);
+            statement.setString(2, sigla);
+            statement.setString(3, telefone);
+            statement.setInt(4, idEndereco);
+            statement.setString(5, matricula);
+
+            return statement.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
     }
 }
 
